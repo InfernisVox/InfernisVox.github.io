5842607a9567eb8ecee3fed816c6a90660047e69
jens <115379514+InfernisVox@users.noreply.github.com>
2023-06-08 18:56:30 +0200

    suffering

--- Concepts/Concept 1/main1.js
+++ Concepts/Concept 1/main1.js
@@ -9,26 +9,27 @@ $(document).ready(function () {
 
 	promise
 		.then(function (winedata) {
-			console.log(winedata);
-			winesByCountryAndVariety(winedata);
+			let winesByVariety = {};
+			let winevarietys = [];
+
+			winesByCountryAndVariety(winedata, winesByVariety, winevarietys);
+			filter(winesByVariety, winevarietys);
+			draw(winesByVariety, winevarietys);
 		})
 		.catch(function (error) {
 			console.log("fail " + error);
 		});
 });
 
-function winesByCountryAndVariety(winedata) {
-	var winesByVariety = {};
-	var winevarietys = [];
-
+function winesByCountryAndVariety(winedata, winesByVariety, winevarietys) {
 	// iterate over all wines and calculate the average values for each variety for each country
 	winedata.forEach(function (wine) {
-		var country = wine.country;
-		var variety = wine.variety;
-		var sweetness = wine.sweetness;
-		var fruitiness = wine.fruitiness;
-		var herbalCount = 0;
-		var fruityCount = 0;
+		let country = wine.country;
+		let variety = wine.variety;
+		let sweetness = wine.sweetness;
+		let fruitiness = wine.fruitiness;
+		let herbalCount = 0;
+		let fruityCount = 0;
 
 		if (!winevarietys.includes(variety)) {
 			var varietyCount = 0;
@@ -47,17 +48,27 @@ function winesByCountryAndVariety(winedata) {
 			if (!winesByVariety[country]) {
 				winesByVariety[country] = {};
 			}
+
 			if (!winesByVariety[country][variety]) {
+				let winePrice = wine.price;
+				if (!Number.isInteger(wine.price)) {
+					winePrice = 0;
+				}
 				winesByVariety[country][variety] = {
 					points: wine.points,
-					price: wine.price,
+					price: winePrice,
 					sweetness: sweetness,
 					fruitiness: fruitiness,
 					count: 1,
+					color: wine.color,
 				};
 			} else {
 				winesByVariety[country][variety].points += wine.points;
-				winesByVariety[country][variety].price += wine.price;
+				if (Number.isInteger(wine.price))
+					winesByVariety[country][variety].price += parseInt(
+						wine.price
+					);
+
 				winesByVariety[country][variety].rating += wine.rating;
 				winesByVariety[country][variety].count++;
 
@@ -109,10 +120,6 @@ function winesByCountryAndVariety(winedata) {
 		}
 	}
 
-	console.log(winesByVariety);
-
-	let elementPositions = [];
-
 	// delete every winevariety that is produced by less than 5 countries
 	for (let i = 0; i < winevarietys.length; i++) {
 		let count = 0;
@@ -144,22 +151,94 @@ function winesByCountryAndVariety(winedata) {
 		}
 		return countB - countA;
 	});
+}
+
+function filter(winesByVariety, winevarietys) {
+	let heading = $("<h1></h1>");
+	let optionlist = ["Points", "Price", "Production"];
+	let filteroption = optionlist[0]; // Initialize filteroption with the first word from the optionlist
+	let previousOption = ""; // Store the previous filter option
+
+	heading.css({
+		position: "absolute",
+		left: "135px",
+		top: "0px",
+		margin: "9px",
+		transform: "translateY(40px)",
+		fontSize: "50px",
+		fontWeight: "bold",
+		color: "white",
+	});
+
+	$("body").append(heading);
+
+	// Create a <u> element for the filteroption
+	let filterElement = $("<u></u>").appendTo(heading);
+
+	// Function to update the filteroption with animation
+	function updateFilterOption() {
+		// Get a random word from the optionlist
+		do {
+			filteroption =
+				optionlist[Math.floor(Math.random() * optionlist.length)];
+		} while (filteroption === previousOption); // Check if it's the same as the previous option
+
+		// Update the previous option
+		previousOption = filteroption;
+
+		// Animate the filteroption change
+		filterElement.animate({ opacity: 0 }, 400, function () {
+			// Update the filteroption text
+			filterElement.text(filteroption).animate({ opacity: 1 }, 400);
+		});
+	}
+
+	// Update the filteroption every few seconds (e.g., every 3 seconds)
+	setInterval(updateFilterOption, 3000);
+
+	// Set the initial heading text
+	heading.html("Winevarietys in <br> the world by ").append(filterElement);
+}
+
+// Function for drawing the entire visualization
+function draw(winesByVariety, winevarietys) {
+	let elementPositions = [];
+	let winecolorboxcolor = "white";
+
+	// change the fontfamily of the body
+	$("body").css("font-family", "Arial, Helvetica, sans-serif");
 
 	$.each(winevarietys, function (index, value) {
 		let append;
+		let kml;
 		$(winevarietys[value]).each(function (index, value) {});
 
-		// p emement
+		if (winesByVariety["US"][value] !== undefined) {
+			kml = winesByVariety["US"][value].color;
+		}
+
+		// change the color behinde the winevariety based on the color of the wine
+		if (kml == "red") {
+			winecolorboxcolor = "rgba(255, 0, 0, 0.2)";
+		} else if (kml == "white") {
+			winecolorboxcolor = "rgba(255, 255, 0, 0.2)";
+		} else if (kml == "not classified") {
+			winecolorboxcolor = "rgba(0, 0, 0, 0.2)";
+		}
+
+		// p element
 		append = $("<p></p>");
 		append.text(value);
 
-		// move down on the y axis by 40px
 		append.css({
 			position: "relative",
-			left: "0px",
-			top: "0px",
-			transform: "translateY(60px)",
+			left: "130px",
+			top: "230px",
+			margin: "9px",
+			transform: "translateY(65px)",
 			fontSize: "12px",
+			fontWeight: "bold",
+			color: "white",
 		});
 
 		$("body").append(append);
@@ -170,6 +249,20 @@ function winesByCountryAndVariety(winedata) {
 			left: position.left,
 			top: position.top,
 		});
+
+		// create the color box
+		let winecolorbox = $("<div></div>");
+		winecolorbox.css({
+			position: "absolute",
+			left: position.left - 8 + "px", // Set the left position of the box to match the text element
+			top: position.top - 4 + "px", // Set the top position of the box to match the text element
+			width: "90%",
+			height: "23.4px",
+			backgroundColor: winecolorboxcolor,
+			zIndex: "-1", // Set a negative z-index to position the box behind the text
+		});
+
+		$("body").append(winecolorbox);
 	});
 
 	// create a container element to hold the <p> elements
@@ -184,13 +277,16 @@ function winesByCountryAndVariety(winedata) {
 		append = $("<p></p>");
 		append.text(index);
 
-		// append.css("position", "absolute");
+		// let the elements be alligned to the x of the circles underneath
 		append.css({
-			position: "relative",
-			margin: "5px",
+			position: "aboslute",
+			margin: "3px",
 			left: "0px",
 			top: "0px",
 			fontSize: "12px",
+			fontWeight: "bold",
+			color: "white",
+			transform: "rotate(-90deg)",
 		});
 
 		// append the <p> element to the container
@@ -201,11 +297,11 @@ function winesByCountryAndVariety(winedata) {
 	// let the elements that are not on the screens be visible by scrolling to the x axis
 	container.css({
 		display: "flex",
-		transform: " ",
 		position: "absolute",
-		top: "30px",
-		right: "10px",
-		left: "170px",
+		height: "30px",
+		top: "265px",
+		right: "0px",
+		left: "343px",
 		textOrientation: "upright",
 		whiteSpace: "nowrap",
 		overflowX: "auto",
@@ -221,10 +317,12 @@ function winesByCountryAndVariety(winedata) {
 	}
 }
 
-function createCircleForWinevariety(element, count, winesByVariety) {
+// Function for creating the circles for each winevariety
+function createCircleForWinevariety(element, counter, winesByVariety) {
 	let circle = $("<div></div>");
 	let circleSize;
 	let saturation;
+	count = counter;
 
 	country = Object.keys(winesByVariety)[count];
 	if (element.variety in winesByVariety[country]) {
@@ -232,7 +330,7 @@ function createCircleForWinevariety(element, count, winesByVariety) {
 			Math.round(winesByVariety[country][element.variety].points),
 			80,
 			100,
-			5,
+			0,
 			35
 		);
 		saturation = Math.round((circleSize / 100) * 255);
@@ -241,13 +339,13 @@ function createCircleForWinevariety(element, count, winesByVariety) {
 	circle.data("country", country);
 
 	circle.css({
-		width: /*circleSize*/ 25 + "px",
-		height: /*circleSize*/ 25 + "px",
-		borderRadius: "0px",
+		width: /*circleSize*/ 22 + "px",
+		height: /*circleSize*/ 22 + "px",
+		borderRadius: "50%",
 		backgroundColor: "hsl(" + saturation + ", 100%, 50%)",
 		position: "absolute",
-		left: 186 + count * 33.5 + "px",
-		top: element.top,
+		left: 350 + count * 30.5 + "px",
+		top: 8.5 + element.top,
 		transform: "translate(-50%, -50%)",
 	});
 
@@ -262,8 +360,8 @@ function createCircleForWinevariety(element, count, winesByVariety) {
 			$(this)
 				.animate(
 					{
-						width: 5 + /*circleSize*/ 25 + "px",
-						height: 5 + /*circleSize*/ 25 + "px",
+						width: 7 + /*circleSize*/ 22 + "px",
+						height: 7 + /*circleSize*/ 22 + "px",
 					},
 					"fast"
 				)
@@ -329,6 +427,7 @@ function createCircleForWinevariety(element, count, winesByVariety) {
 	);
 }
 
+// Map function
 function map(number, inMin, inMax, outMin, outMax) {
 	return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
